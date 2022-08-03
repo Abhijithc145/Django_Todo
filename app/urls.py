51e400dc7e5e5327bfafcb789e9098ac5ec1849c
Abhijith <abhijithabhipgmr145@gmail.com>
2022-08-03 14:21:09 +0530

    Finish todo

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from  . import views
 
 urlpatterns = [
-    path('signup',views.signup,name='signup'),
+    path('',views.index,name='index'),
+    path('Delete/<str:pk>',views.Delete,name='Delete'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import *
 # Create your views here.
+def index(request):
+    todo = Url.objects.all()
+    if request.method == 'POST':
+        new_Todu = Url(title = request.POST['title']).save()
+        redirect('/')
+    return render(request,'index.html',{'todo':todo})
+
+def Delete(request,pk):
+    todo = Url.objects.get(id = pk)
+    todo.delete()
+    return redirect('/')
